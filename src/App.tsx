13da2cec5d2f611d93c15e13cabffdd6f3e825db
Armand Nahimana <57989037+Trezzmann@users.noreply.github.com>
2022-02-13 16:35:59 +0200

    IncomeExpenses component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Header from "./components/Header";
 import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Balance />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </div>
   );
 }
