d18269523aafa6253d8c4f6b954bc0c487b6cfe6
Armand Nahimana <57989037+Trezzmann@users.noreply.github.com>
2022-02-13 16:32:50 +0200

    balance component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Balance from "./components/Balance";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Balance />
     </div>
   );
 }
