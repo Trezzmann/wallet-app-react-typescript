558d7d9c922e520915645266330f37f0f3f71458
Armand Nahimana <57989037+Trezzmann@users.noreply.github.com>
2022-02-13 17:10:37 +0200

    calculate income & expenses

--- src/components/IncomeExpenses.tsx
+++ src/components/IncomeExpenses.tsx
@@ -1,16 +1,31 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppState";
+
 export default function IncomeExpenses() {
+  const { transactions } = useContext(AppContext);
+
+  const income = transactions
+    .filter((x) => x.amount > 0)
+    .reduce((acc, val) => acc + val.amount, 0)
+    .toFixed(2);
+
+  const expense = transactions
+    .filter((x) => x.amount < 0)
+    .reduce((acc, val) => acc + val.amount * -1, 0)
+    .toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
         <p id="money-plus" className="money plus">
-          +$0.00
+          +${income}
         </p>
       </div>
       <div>
         <h4>Expense</h4>
         <p id="money-minus" className="money minus">
-          -$0.00
+          -${expense}
         </p>
       </div>
     </div>
