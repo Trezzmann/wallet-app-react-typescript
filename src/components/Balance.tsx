a32ed2fab3e62427432d16a716836f57beb30903
Armand Nahimana <57989037+Trezzmann@users.noreply.github.com>
2022-02-13 17:06:23 +0200

    calculate total amount

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,8 +1,17 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppState";
+
 export default function Balance() {
+  const { transactions } = useContext(AppContext);
+  const total = transactions
+    .map((x) => x.amount)
+    .reduce((acc, val) => acc + val, 0)
+    .toFixed(2);
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1 id="balance">$0.00</h1>
+      <h1 id="balance">${total}</h1>
     </>
   );
 }
